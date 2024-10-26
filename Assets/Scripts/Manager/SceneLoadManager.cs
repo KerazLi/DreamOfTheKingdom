ce338e2f3603137791bc061e4057e253fcbff6c8
Keraz <1530217461@qq.com>
2024-10-27 01:55:47 +0800

    房间进出逻辑

--- Assets/Scripts/Manager/SceneLoadManager.cs
+++ Assets/Scripts/Manager/SceneLoadManager.cs
@@ -1,14 +1,79 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
+using static UnityEngine.SceneManagement.LoadSceneMode;
 
 public class SceneLoadManager : MonoBehaviour
 {
-    public void OnLoadRoomEvent(object data)
+
+    private AssetReference currentScene;
+    public AssetReference map;
+    /// <summary>
+    /// 处理加载房间事件时的行为
+    /// </summary>
+    /// <param name="data">传递进来的数据，可以是任何类型，但预期是RoomDataSO类型</param>
+    public async void OnLoadRoomEvent(object data)
     {
+        // 检查传入的数据是否为RoomDataSO类型，以确保类型安全
         if (data is RoomDataSO)
         {
-            var currentData= (RoomDataSO)data ;
-            Debug.Log(currentData.roomType);
+            // 将data转换为RoomDataSO类型，以便访问其属性
+            var currentData = (RoomDataSO)data;
+            // 输出房间类型信息，用于调试目的
+            //Debug.Log(currentData.roomType);
+            currentScene = currentData.sceneToLoad;
         }
+        //卸载房间
+        await UnloadSceneTask();
+        //加载房间
+        await  LoadSceneTask();
+    }
+    
+    /// <summary>
+    /// 异步加载场景任务。
+    /// </summary>
+    /// <returns>返回一个可等待的任务。</returns>
+    private async Awaitable LoadSceneTask()
+    {
+        // 启动当前场景的异步加载任务，使用 additive 模式加载场景，以便将新场景与当前场景合并
+        var s=currentScene.LoadSceneAsync(LoadSceneMode.Additive);
+        
+        // 等待异步加载任务完成
+        await s.Task;
+        
+        // 检查加载任务是否成功完成
+        if (s.Status==AsyncOperationStatus.Succeeded)
+        {
+            // 如果加载成功，则将新加载的场景设置为活动场景
+            SceneManager.SetActiveScene(s.Result.Scene);
+        }
+    }
+    
+    /// <summary>
+    /// 异步卸载当前激活的场景。
+    /// </summary>
+    /// <returns>返回一个Awaitable对象，用于等待场景卸载完成。</returns>
+    private async Awaitable UnloadSceneTask()
+    {
+        // 使用场景管理器的异步方法卸载当前激活的场景
+        await SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());   
+    }
+
+    /// <summary>
+    /// 异步加载地图场景。
+    /// </summary>
+    /// <returns>无返回值。</returns>
+    public async void LoadMap()
+    {
+        // 等待当前场景卸载完成，以避免场景间的冲突。
+        await UnloadSceneTask();
+        
+        // 设置当前场景为地图，准备加载新的地图场景。
+        currentScene = map;
+        
+        // 等待新场景加载完成，确保场景切换的流畅性。
+        await LoadSceneTask();
     }
 
 }
