a00ae562e586a50bb11265ac4078b03b55a7c4ea
Keraz <1530217461@qq.com>
2024-11-06 15:58:29 +0800

    导入免费素材

---
+++ Assets/Scripts/Cards/MonoBehaviour/Card.cs
@@ -0,0 +1,35 @@
+using System;
+using Cards.ScriptObject;
+using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [Header("组件")] 
+    public SpriteRenderer cardSprite;
+
+    public TextMeshPro costText, descriptionText, typeText;
+    
+    public CardDataSO cardData;
+
+    private void Start()
+    {
+        InintCardData(cardData);
+    }
+
+    public void InintCardData(CardDataSO cardData)
+    {
+        //cardData = data;
+        cardSprite.sprite = cardData.cardImage;
+        costText.text = cardData.cost.ToString();
+        descriptionText.text = cardData.cardDescription;
+        typeText.text = cardData.cardType switch
+        {
+            CardType.Attack => "攻击",
+            CardType.Defense => "防御",
+            CardType.Skill => "技能",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+}
---
+++ Assets/Scripts/Cards/ScriptObject/CardDataSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Cards.ScriptObject
+{
+    [CreateAssetMenu(fileName = "CardDataSO", menuName = "Card/CardDataSO", order = 0)]
+    public class CardDataSO : ScriptableObject
+    {
+       public string cardName;
+       public Sprite cardImage;
+       public int cost;
+       public CardType cardType;
+       [TextArea]
+       public string cardDescription;
+       //TODO:执行效果
+    }
+}
